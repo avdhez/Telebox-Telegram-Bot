9ad095a1d435fb5b1653ab49d7820a9ba15181fb
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-29 17:40:18 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,7 @@
+# app/config.py
+class Config:
+    TELEGRAM_API_TOKEN = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+    TELEBOX_API = 'YOUR_TELEBOX_API_TOKEN'
+    TELEBOX_BASEFOLDER = 'YOUR_TELEBOX_BASE_FOLDER_ID'
+    TELEBOX_BASE_URI = 'https://www.linkbox.to/'
+    USR_LIMIT_CONCURRENT = 5  # Limit the number of concurrent uploads/downloads
