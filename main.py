10e35f11ff6370af585637f39ac7ea37b2f957f2
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-30 18:00:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from tqdm import tqdm
 
-TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-TELEBOX_API_KEY = 'YOUR_TELEBOX_API_KEY'
+TOKEN = '7086505429:AAEXdqa_LA2shyxeW1jd6WKlJwMcJ-W6T6E'
+TELEBOX_API_KEY = '24lAbSDb9KbMTz8K'
 
 def download_file(update, context):
     file_id = update.message.document.file_id
