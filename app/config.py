54de106b32010edf04796c4618be83f3ddcaa84a
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-30 17:03:15 +0530

    Delete app directory

--- app/config.py
+++
@@ -1,7 +0,0 @@
-# app/config.py
-class Config:
-    TELEGRAM_API_TOKEN = '7086505429:AAEXdqa_LA2shyxeW1jd6WKlJwMcJ-W6T6E'
-    TELEBOX_API = '24lAbSDb9KbMTz8K'
-    TELEBOX_BASEFOLDER = '47020617'
-    TELEBOX_BASE_URI = 'https://www.linkbox.to/'
-    USR_LIMIT_CONCURRENT = 5  # Limit the number of concurrent uploads/downloads
