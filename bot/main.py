7d078186b49b9a08f87cea2dca452ff19cd9b5a7
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-30 17:04:03 +0530

    Update

---
+++ bot/main.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater
+from bot.handlers import start, handle_file, handle_text
+from bot.config import TELEGRAM_BOT_TOKEN
+
+def main():
+    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.document, handle_file))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
