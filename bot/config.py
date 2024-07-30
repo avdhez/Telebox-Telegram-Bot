a4d197ab90361efca823c65c946fe4bea0fc84dd
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-30 17:55:33 +0530

    Delete bot directory

--- bot/config.py
+++
@@ -1,9 +0,0 @@
-# Configuration for API tokens and endpoints
-
-TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-TELEBOX_API_TOKEN = 'YOUR_TELEBOX_API_TOKEN'
-
-UPLOAD_AUTH_URL = 'https://www.linkbox.to/api/open/get_upload_url'
-UPLOAD_FILE_URL = 'https://www.linkbox.to/api/open/folder_upload_file'
-SHARE_FILE_URL = 'https://www.linkbox.to/api/open/file_share'
-DOWNLOAD_FILE_URL = 'https://www.linkbox.to/api/open/file_search'
