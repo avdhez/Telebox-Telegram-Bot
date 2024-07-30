f0d8c1d3932e832c9ab2259b58674104c0510062
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-07-30 07:22:04 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 # app/config.py
 class Config:
-    TELEGRAM_API_TOKEN = 'YOUR_TELEGRAM_BOT_API_TOKEN'
-    TELEBOX_API = 'YOUR_TELEBOX_API_TOKEN'
-    TELEBOX_BASEFOLDER = 'YOUR_TELEBOX_BASE_FOLDER_ID'
+    TELEGRAM_API_TOKEN = '7086505429:AAEXdqa_LA2shyxeW1jd6WKlJwMcJ-W6T6E'
+    TELEBOX_API = '24lAbSDb9KbMTz8K'
+    TELEBOX_BASEFOLDER = '47020617'
     TELEBOX_BASE_URI = 'https://www.linkbox.to/'
     USR_LIMIT_CONCURRENT = 5  # Limit the number of concurrent uploads/downloads
